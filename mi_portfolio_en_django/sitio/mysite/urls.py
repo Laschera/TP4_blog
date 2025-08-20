f03b7c30f903d9a860bbfc84e64406fb0ef4f89c
Laschera <fdlaschera@gmail.com>
2025-08-19 21:29:15 -0300

    comentarios del blog y que modo oscuro se guarde en portfolio/blog

--- mi_portfolio_en_django/sitio/mysite/urls.py
+++ mi_portfolio_en_django/sitio/mysite/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     path('post/new/', view.post_new, name='post_new'),
     path('post/<int:pk>/', view.post_detail, name='post_detail'),
     path('post/<int:pk>/edit/', view.post_edit, name='post_edit'),
+    path('post/<int:pk>/comment/', view.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', view.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', view.comment_remove, name='comment_remove'),
 ]
 
 #from django.contrib import admin
